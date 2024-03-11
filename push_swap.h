42dff4a3ef40bed3fab034079a645ddae80110f2
Lim Chee Tian <lchee-ti@u91z01s04.42kl.edu.my>
2024-03-11 16:53:40 +0800

    moves

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:16:04 by lchee-ti          #+#    #+#             */
-/*   Updated: 2024/03/07 06:57:00 by codespace        ###   ########.fr       */
+/*   Updated: 2024/03/11 16:22:08 by lchee-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,66 +30,5 @@ typedef struct s_stack_node
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;	
 }					t_stack_node;
-
-typedef struct s_moves
-{
-	int				ra;
-	int				rb;
-	int				rra;
-	int				rrb;
-}					t_moves;
-
-typedef struct s_data
-{
-	t_stack			*a;
-	t_stack			*b;
-	t_moves			*moves;
-	int				*sorted;
-	int				size;
-}					t_data;
-
-void				ft_swap(t_stack *stack);
-void				ft_push(t_stack *stack1, t_stack *stack2);
-void				ft_rotate(t_stack *stack);
-void				ft_reverse_rotate(t_stack *stack);
-
-void				ft_swap_a(t_data *data);
-void				ft_swap_b(t_data *data);
-void				ft_swap_s(t_data *data);
-void				ft_push_a(t_data *data);
-void				ft_push_b(t_data *data);
-void				ft_rotate_a(t_data *data);
-void				ft_rotate_b(t_data *data);
-void				ft_rotate_r(t_data *data);
-void				ft_reverse_rotate_a(t_data *data);
-void				ft_reverse_rotate_b(t_data *data);
-void				ft_reverse_rotate_r(t_data *data);
-
-void				ft_init_data(t_data *data, int size);
-void				ft_init_stack(t_stack *stack, int size);
-void				ft_init_moves(t_moves *moves);
-void				ft_init_sorted(t_data *data);
-
-void				ft_free_data(t_data *data);
-void				ft_free_stack(t_stack *stack);
-void				ft_free_moves(t_moves *moves);
-void				ft_free_sorted(t_data *data);
-
-void				ft_check_duplicates(t_data *data);
-void				ft_check_sorted(t_data *data);
-void				ft_check_sorted_b(t_data *data);
-void				ft_check_sorted_a(t_data *data);
-
-void				ft_push_swap(t_data *data);
-void				ft_sort(t_data *data);
-void				ft_sort_3(t_data *data);
-void				ft_sort_5(t_data *data);
-void				ft_sort_100(t_data *data);
-void				ft_sort_500(t_data *data);
-
-void				ft_error(void);
-void				ft_error_free(t_data *data);
-void				ft_error_free_exit(t_data *data);
-void				ft_error_free_exit_str(t_data *data, char *str);
 
 #endif
---
+++ split.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 12:24:32 by lchee-ti          #+#    #+#             */
+/*   Updated: 2024/03/11 12:54:51 by lchee-ti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_word_count(char const *s, char c)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+static char	*ft_word_dup(char const *s, char c)
+{
+	int		i;
+	char	*word;
+	int		len;
+
+	i = 0;
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	char	**split;
+
+	i = 0;
+	j = 0;
+	split = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!split)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			split[j] = ft_word_dup(&s[i], c);
+			if (!split[j])
+				return (NULL);
+			j++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	split[j] = NULL;
+	return (split);
+}
