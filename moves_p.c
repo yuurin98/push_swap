2e06372736158062e205212b576743776b8e4391
Lim Chee Tian <lchee-ti@u91z05s02.42kl.edu.my>
2024-03-13 14:49:32 +0800

    moves

--- moves_p.c
+++ moves_p.c
@@ -6,7 +6,7 @@
 /*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:13:04 by lchee-ti          #+#    #+#             */
-/*   Updated: 2024/03/13 14:13:16 by lchee-ti         ###   ########.fr       */
+/*   Updated: 2024/03/13 14:48:25 by lchee-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,27 @@ void pa(t_stack *stack_a, t_stack *stack_b)
 	}
 	stack_a->size++;
 	stack_b->size--;
+}
+
+void	pb(t_stack *stack_a, t_stack *stack_b)
+{
+	int	i;
+
+	if (stack_a->size == 0)
+		return ;
+	i = stack_b->size;
+	while (i > 0)
+	{
+		stack_b->tab[i] = stack_b->tab[i - 1];
+		i--;
+	}
+	stack_b->tab[0] = stack_a->tab[0];
+	i = 0;
+	while (i < stack_a->size)
+	{
+		stack_a->tab[i] = stack_a->tab[i + 1];
+		i++;
+	}
+	stack_b->size++;
+	stack_a->size--;
 }
