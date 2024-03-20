d1981872d5e3711768e4f3ced33ac5cdfa8f8f6b
Lim Chee Tian <lchee-ti@u91z06s04.42kl.edu.my>
2024-03-20 17:37:24 +0800

    b

--- moves_s.c
+++ moves_s.c
@@ -6,7 +6,7 @@
 /*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 12:24:23 by lchee-ti          #+#    #+#             */
-/*   Updated: 2024/03/20 17:16:54 by lchee-ti         ###   ########.fr       */
+/*   Updated: 2024/03/20 17:26:50 by lchee-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,5 @@ void	ss(t_stack *stack_a, t_stack *stack_b)
 {
 	sa_quiet(stack_a);
 	sb_quiet(stack_b);
+	ft_printf("ss\n")
 }
